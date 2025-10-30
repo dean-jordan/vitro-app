962adb1694029d9f037475010eb9a11f64d8daa0
dean-jordan <jordan.dean161@gmail.com>
2025-10-30 01:41:01 -0400

    Update Community.py

--- pages/Community.py
+++ pages/Community.py
@@ -1,42 +1,15 @@
-from openai import OpenAI
 import streamlit as st
 
 st.title("The Vitro Community")
 st.caption("See the genomes others have created and contribute to the open research community.")
 
-st.form(
-    key="form1",
-    clear_on_submit="True",
-    enter_to_submit=True,
-    border=True,
-)
+with st.form(key="form1", clear_on_submit=True):
+    name = st.text_input(label="Name", key="name")
+    post_title = st.text_input(label="Post Title", key="post_title")
+    gen_prefix = st.text_area(label="Generated Prefix", key="gen_prefix")
+    additional = st.text_area(label="Additional Generated Materials", key="additional")
+    description = st.text_area(label="Description of Findings/Problem", key="description")
+    submitted = st.form_submit_button(label="Submit")
 
-st.text_area(
-    label="Name",
-    key="form1"
-)
-
-st.text_area(
-    label="Post Title",
-    key="form1"
-)
-
-st.text_area(
-    label="Generated Prefix",
-    key="form1"
-)
-
-st.text_area(
-    label="Additional Generated Materials",
-    key="form1"
-)
-
-st.text_area(
-    label="Description of Findings/Problem",
-    key="form1"
-)
-
-st.form_submit_button(
-    label="Submit",
-    key="form1"
-)
+    if submitted:
+        st.success("Thank you — your post has been submitted.")
