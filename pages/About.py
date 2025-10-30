fc06e0d83d4e9a93f58bb3fc39cd2abd38e6d867
dean-jordan <jordan.dean161@gmail.com>
2025-10-30 12:40:49 -0400

    Finished about section

--- pages/About.py
+++ pages/About.py
@@ -10,8 +10,8 @@ st.text("Vitro is a tool for creating DNA prefixes. DNA prefixes are extremely u
 
 st.header("AlphaFold 2/AlphaFold 3 Integration")
 
-st.text("")
+st.text("To visualize generated prefixes or full sequences (generated with Evo 2 and Vitro) on AlphaFold 3, simply use the link on the sidebar to reach the AlphaFold Server. The AlphaFold Server is a cloud-based platform for visualizing proteins, DNA, and RNA sequences. Note that a standard account is limited to 30 visualizations per day, however this quota is sufficient for Vitro. Note that Vitro contains full compatibility with AlphaFold 2 as it was trained on the NCBI GenBank dataset, allowing for full biological plausibility.")
 
 st.header("Use of Prefixes for Evo/Evo 2")
 
-st.text("")
+st.text("One of the most crucial applications for a generated prefix is creating a full DNA sequence. This can be done with Evo 2. The link in the sidebar directs to the Evo 2 Server, which can be used to add nucleotides to the prefixes to create novel life. As Vitro is trained on NCBI GenBank's annotated genomes, an accuracy of ~92 percent is observed, hence being highly accurate. This means that Vitro contains full compatibility with Evo 2 alongside AlphaFold. Note that generated sequences may not be fully optimized. However, Evo 2/Vitro sequences will be fully biologically plausible.")
